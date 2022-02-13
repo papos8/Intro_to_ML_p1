01561c078a2c428f5d5f5516febd8f769ea54f5f
papos8 <panagiotispapadamos@gmail.com>
2022-02-13 13:06:19 +0100

    Create python files

---
+++ data_preprocessing.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+"""
+Load data from MaternalDataset.csv and preprocess them
+
+"""
---
+++ project1.py
@@ -0,0 +1,12 @@
+import imp
+
+
+import numpy as np
+import pandas as pd
+from data_preprocessing import *
+
+"""
+Once we have load the data we visualize and apply PCA
+
+
+"""
