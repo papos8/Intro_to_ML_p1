8f169a65e51234e4826fabbbbbfa218522b8acbd
papos8 <panagiotispapadamos@gmail.com>
2022-02-22 14:44:54 +0100

    Upload the statistics

---
+++ statistics.py
@@ -0,0 +1,45 @@
+#This file contains the summary of statistics for our dataset
+import math
+from data_preprocessing import *
+
+mean = []
+
+#Transpose data to easier access the values
+t_data = data.T
+print(t_data)
+
+K,L=t_data.shape
+
+
+#Create an array for the mean of the attributes
+mean = []
+for i in range(K):
+    mean.append(round(t_data[i].mean(),2))
+print("The array of the mean values is: " , mean)
+
+#Create an array for the variance of the attributes 
+var = []
+for i in range(K):
+    var.append(round(t_data[i].var(),2))
+print("The array of the variance is: " , var)
+
+#Create an array for the svd of the attributes
+svd = []
+for i in range(K):
+    svd.append(round(math.sqrt(var[i]),2))
+print("The array of the standard deviation is: " , svd)
+
+#Create an array with the range of the values
+v_range = []
+for i in range(K):
+    v_range.append(t_data[i].max()-t_data[i].min())
+print("The array of the range is: " , v_range)
+
+#Determine mix and max
+v_min = []
+v_max = []
+for i in range(K):
+    v_min.append(t_data[i].min())
+    v_max.append(t_data[i].max())
+print("The array of the min values is: " , v_min)
+print("The array of the max values is: " , v_max)
