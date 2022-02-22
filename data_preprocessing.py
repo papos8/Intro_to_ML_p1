1c37a431f5cd63aaf7967768e0d7e47b8cdba4a0
Dimitrios Loukas <dimitrisloukas879@gmail.com>
2022-02-22 20:57:46 +0100

    Plot
    This reverts commit 8f169a65e51234e4826fabbbbbfa218522b8acbd.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -9,11 +9,18 @@ Load data from MaternalDataset.csv and preprocess them
 
 #Load the data in pandas dataframe
 df = pd.read_csv("./MaternalDataset.csv")
+attributeNames = ["Age", "Systolic BP", "Diastolic BP", "Blood Glucose", 
+                  "Body Temperature", "Heart Rate", "Risk Level"]
 raw_data = df.values
+classes = raw_data[:-2,-1]
+classes = list(set(classes))
+
+classNames = {"low risk":1,"mid risk":2, "high risk":3}
+y = np.asarray([classNames[value] for value in raw_data[:,-1]])
+
 N,M = raw_data.shape
 data = np.asarray(raw_data)
 
-print(raw_data)
 
 #Transform the values of the last attribute to numerical
 for i in range(N):
@@ -31,6 +38,8 @@ for i in range(N):
         if not (isinstance(data[i,j],int) or isinstance(data[i,j],float)):
             count = count + 1
 
-print("The number of missing of corrupted values is %i." %count)
-print(data)
+# Uncomment
+#print("The number of missing of corrupted values is %i." %count)
+#print(data)
+
 
--- project1.py
+++ project1.py
@@ -1,12 +1,26 @@
-import imp
-
-
 import numpy as np
 import pandas as pd
 from data_preprocessing import *
+import matplotlib.pyplot as plt
 
 """
 Once we have load the data we visualize and apply PCA
 
 
 """
+plt.figure(figsize=(10,10))
+k=1
+for i in range(7):    
+    for j in range(7):
+        plt.subplot(7,7,k)
+        for c in range(1,len(classes)+1):
+                # select indices belonging to class c:
+                class_mask = y==c                   
+                plt.plot(data[class_mask,i], data[class_mask,j], 'o',alpha=1)
+                plt.legend(classNames)
+                plt.xlabel(attributeNames[i])
+                plt.ylabel(attributeNames[j])
+        k=k+1   
+            
+           
+            
