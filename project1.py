17b7533402c8d4cdccce1d0cc72d641eeec71b94
Dimitrios Loukas <dimitrisloukas879@gmail.com>
2022-02-26 18:17:50 +0100

    Added  mean substraction and SVD

--- project1.py
+++ project1.py
@@ -2,13 +2,14 @@ import numpy as np
 import pandas as pd
 from data_preprocessing import *
 import matplotlib.pyplot as plt
+from scipy.linalg import svd
 
 """
 Once we have load the data we visualize and apply PCA
 
 
 """
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(40,40))
 k=1
 for i in range(7):    
     for j in range(7):
@@ -21,6 +22,19 @@ for i in range(7):
                 plt.xlabel(attributeNames[i])
                 plt.ylabel(attributeNames[j])
         k=k+1   
-            
            
-            
+#Create X matrix from raw_data
+X = np.empty((1014, 7))
+for i, col_id in enumerate(range(1,8)):
+    X[:, i] = np.asarray(raw_data[:,col_id-1])
+
+#Substract the mean / SVD
+Y = X - np.ones((N,1))*X.mean(axis=0)     
+U,S,V = svd(Y,full_matrices=False)          
+rho = (S*S) / (S*S).sum() 
+threshold = 0.9
+
+# Compute values of N, M and C.
+N = len(y)
+M = len(attributeNames)
+C = len(classNames)
