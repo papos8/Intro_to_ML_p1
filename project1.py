2e99878dd7663ab2b706d9219d79ed65f379854e
Dimitrios Loukas <dimitrisloukas879@gmail.com>
2022-02-26 18:30:06 +0100

    Added PCA (PC1 vs PC2)

--- project1.py
+++ project1.py
@@ -38,3 +38,43 @@ threshold = 0.9
 N = len(y)
 M = len(attributeNames)
 C = len(classNames)
+
+#plot variance explained
+plt.figure()
+plt.plot(range(1,len(rho)+1),rho,'x-')
+plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
+plt.plot([1,len(rho)],[threshold, threshold],'k--')
+plt.title('Variance explained by principal components');
+plt.xlabel('Principal component');
+plt.ylabel('Variance explained');
+plt.legend(['Individual','Cumulative','Threshold'])
+plt.grid()
+plt.show()
+
+# PCA by computing SVD of Y
+U,S,Vh = svd(Y,full_matrices=False)
+
+V = Vh.T   
+
+# Project the centered data onto principal component space
+Z = Y @ V
+
+# Indices of the principal components to be plotted
+i = 0
+j = 1
+
+# Plot PCA of the data
+f = plt.figure()
+plt.title('Maternal Health Risks: PCA')
+#Z = array(Z)
+for c in range(3):
+    # select indices belonging to class c:
+    class_mask = y==c+1
+    plt.plot(Z[class_mask,i], Z[class_mask,j], 'o', alpha=.5)
+    print(Z[class_mask,i])
+plt.legend(classNames)
+plt.xlabel('PC{0}'.format(i+1))
+plt.ylabel('PC{0}'.format(j+1))
+
+# Output result to screen
+plt.show()
