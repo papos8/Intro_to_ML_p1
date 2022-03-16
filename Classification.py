f021b30a860704beca25453117b0c79e4b3eb949
Dimitrios Loukas <dimitrisloukas879@gmail.com>
2022-03-16 16:39:01 +0100

    Added KNN and Baseline classification

--- Classification.py
+++ Classification.py
@@ -3,9 +3,13 @@ from project1 import *
 import seaborn as sns
 import pandas as pd
 import matplotlib as plt
-from matplotlib.pylab import figure, plot, subplot, xlabel, ylabel, hist, show
+from matplotlib.pylab import figure, plot, subplot, xlabel, ylabel, hist, show, scatter
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+
+
+#LINEAR REGRESSION
+
 
 #Prepare the data - Input/Output
 y = X[:,6]
@@ -28,5 +32,18 @@ y_est = model.predict(X_test)
 #Residual
 residual = y_est-y_test
 
+#BASELINE CLASSIFICATION
 
-np.count_nonzero(y==3) + np.count_nonzero(y==2) + np.count_nonzero(y==1)
+
+#Find the class with the most observations
+max_class = 0
+for i in y:
+    np.count_nonzero(y==i)
+    if np.count_nonzero(y==i) > max_class:
+        max_class = np.count_nonzero(y==i)
+        max_class_id = i
+
+baseline_accuracy = max_class / len(y)
+
+scatter(X[:,5],X[:,4],X[:,3],X[:,1],c=y)
+
